44d3f9a842f28d545bafa6220b4d01103c1cc9a1
1312653 <1312653@student.hcmus.edu.vn>
2016-05-03 16:19:00 +0700

    tao ket noi

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+
+
+
+
+
+
+
+
+
+// launch ======================================================================
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
